8494ed3642560081730365dfac4ca9a41fc6fd0e
ben_xamariners <ben@xamariners.com>
2019-09-02 23:42:11 +0800

    initial commit

---
+++ XamForms.Bdd.Test/Infrastructure/StepBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using TechTalk.SpecFlow;
+using XamForms.Bdd.ViewModels;
+
+namespace XamForms.Bdd.Test.Infrastructure
+{
+    public class StepBase
+    {
+        protected readonly ScenarioContext ScenarioContext;
+
+        public StepBase(ScenarioContext scenarioContext)
+        {
+            ScenarioContext = scenarioContext;
+        }
+
+        public TViewModel GetCurrentViewModel<TViewModel>() where TViewModel : class
+        {
+            return SetupHooks.Application?.MainPage?.Navigation?.NavigationStack?
+                    .LastOrDefault()?.BindingContext as TViewModel;
+        }
+    }
+}
---
+++ XamForms.Bdd/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmHelpers;
+
+namespace XamForms.Bdd.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+    }
+}
